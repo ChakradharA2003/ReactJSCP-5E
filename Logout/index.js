750ed4d370a698ec213b5d0c837385bcc563a38a
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2023-12-07 23:31:20 +0530

    Create index.js

---
+++ Logout/index.js
@@ -0,0 +1,11 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const LogoutButton = () => (
+  <button type="button" className="button">
+    Logout
+  </button>
+)
+
+export default LogoutButton
