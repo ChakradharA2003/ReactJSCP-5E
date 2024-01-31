3cc197a5e01b35a008af0d03f1ebfba1f2fddfbe
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-01-31 22:47:29 +0530

    Update index.js

--- Login/index.js
+++ Login/index.js
@@ -1,10 +1,16 @@
 // Write your code here
+// Write your code here
 import './index.css'
 
-const LoginButton = () => (
-  <button type="button" className="button">
-    Login
-  </button>
-)
-
+const LoginButton = props => {
+  const {onClickButton} = props
+  const changeState = () => {
+    onClickButton()
+  }
+  return (
+    <button type="button" className="button" onClick={changeState()}>
+      Login
+    </button>
+  )
+}
 export default LoginButton
